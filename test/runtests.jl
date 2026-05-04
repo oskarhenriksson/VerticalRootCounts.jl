bc0407b941e035f6281c34d3eacd6ef38d2c54cd
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2026-05-04 23:59:43 +0200

    fix: simplify runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,5 @@
 using Test
-using Documenter
 using VerticalRootCounts
-
-
-DocMeta.setdocmeta!(VerticalRootCounts, :DocTestSetup,
-    :(using VerticalRootCounts, Oscar, Catalyst),
-    recursive = true,
-)
-
-doctest(VerticalRootCounts, manual = false)
 
 include("test_root_bounds.jl")
 include("test_crnt.jl")
